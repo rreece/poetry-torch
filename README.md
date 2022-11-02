c89e7db2870cac0187e1801fec3cbc4a73628a8f
Ryan Reece <ryan.d.reece@live.com>
2022-11-02 16:43:27 -0700

    add poetry-torch

---
+++ README.md
@@ -0,0 +1,13 @@
+# poetry-torch
+
+## How to setup
+
+```
+poetry new poetry-torch
+cp poetry-torch-bak/poetry.toml .
+poetry add numpy
+poetry add torch --platform linux --python "^3.8"
+poetry add transformers
+```
+
+
---
+++ tests/test_hf_bert_sentiment.py
@@ -0,0 +1,20 @@
+"""
+Test Huggingface pretrained BERT.
+"""
+
+
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+
+tokenizer = BertTokenizer.from_pretrained("textattack/bert-base-uncased-yelp-polarity")
+model = BertForSequenceClassification.from_pretrained("textattack/bert-base-uncased-yelp-polarity")
+
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+predicted_class_id = logits.argmax().item()
+result = model.config.id2label[predicted_class_id]
+
+print(result)
